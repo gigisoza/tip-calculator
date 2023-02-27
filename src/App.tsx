245ddb98eb0b8452902fc73bba830314f1b27f45
gigisoza <sozashviligigi@gmail.com>
2023-02-27 22:46:53 +0400

    Lock decimals in number of people input

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,30 @@ function App() {
   const [people, setPeople] = useState<number>(0);
   const [tip, setTip] = useState<number>(0);
 
-  console.log(bill, people, tip);
+  const tipAmount = (bill * tip / people).toFixed(2);
+  const totalPerPerson = ((bill * (1 + tip) / people).toFixed(2));
+  const showTip = !(tipAmount === 'NaN' || tipAmount === 'Infinity');
+  const showTotal = !(totalPerPerson === 'NaN' || totalPerPerson === 'Infinity');
 
   return (
     <div className="App">
-      <div>bill: {bill}</div>
-      <div>people: {people}</div>
-      <div>people: {tip}</div>
       <input 
         placeholder="bill"
         type="number"
+        value={bill}
         onChange={(e) => {
           setBill(e.target.valueAsNumber)
         }}
       />
+
+      <button
+        onClick={() => {
+          setTip(0.05)
+        }}
+      >
+        5%
+      </button>
+
       <button
         onClick={() => {
           setTip(0.1)
@@ -27,18 +37,52 @@ function App() {
       >
         10%
       </button>
+
+      <button
+        onClick={() => {
+          setTip(0.15)
+        }}
+      >
+        15%
+      </button>
+
+      <button
+        onClick={() => {
+          setTip(0.25)
+        }}
+      >
+        25%
+      </button>
+
+      <button
+        onClick={() => {
+          setTip(0.5)
+        }}
+      >
+        50%
+      </button>
+
       <input 
         placeholder="number of people"
         type="number"
+        value={people}
+        onKeyDown={(e) => {
+          if (e.key === '.'){
+            e.preventDefault();
+          }
+        }}
         onChange={(e) => {
+          console.log(e.target.value);
           setPeople(e.target.valueAsNumber);
         }}
       />
       <div>
-        tip amount / person: {(bill * tip / people).toFixed(2)}
+        tip amount / person: 
+        {showTip ? tipAmount : "0.00"}
       </div>
       <div>
-        total / person: {(bill * (1 + tip) / people).toFixed(2)}
+        total / person: 
+        {showTotal ? totalPerPerson : "0.00"}
       </div>
     </div>
   );
