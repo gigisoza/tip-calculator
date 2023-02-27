0ccfcbb68b92b1a3d657b131bafad355a4567635
gigisoza <sozashviligigi@gmail.com>
2023-02-27 21:42:35 +0400

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [bill, setBill] = useState<number>(0);
+  const [people, setPeople] = useState<number>(0);
+  const [tip, setTip] = useState<number>(0);
+
+  console.log(bill, people, tip);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>bill: {bill}</div>
+      <div>people: {people}</div>
+      <div>people: {tip}</div>
+      <input 
+        placeholder="bill"
+        type="number"
+        onChange={(e) => {
+          setBill(e.target.valueAsNumber)
+        }}
+      />
+      <button
+        onClick={() => {
+          setTip(0.1)
+        }}
+      >
+        10%
+      </button>
+      <input 
+        placeholder="number of people"
+        type="number"
+        onChange={(e) => {
+          setPeople(e.target.valueAsNumber);
+        }}
+      />
+      <div>
+        tip amount / person: {(bill * tip / people).toFixed(2)}
+      </div>
+      <div>
+        total / person: {(bill * (1 + tip) / people).toFixed(2)}
+      </div>
     </div>
   );
 }
