093032331bb70ed8050b1ceded92047093d73755
gigisoza <sozashviligigi@gmail.com>
2023-03-07 05:20:51 +0400

    Organize tip buttons

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import { Input } from './components/Input';
 import { defaultTheme } from './themes/defaultTheme';
 import Logo from './images/logo.svg';
 import Label from './components/Label';
-import ResetButton from './components/ResetButton';
-import { BillName, Bill, PerPerson } from './components/Bill';
+import Tip from './components/Tip';
+import Results from './components/Results';
 
 
 function App() {
@@ -18,8 +18,8 @@ function App() {
 
 
   const alright = bill !== undefined && people !== undefined && tip !== undefined;
-  const tipAmount = alright && (bill * tip / people).toFixed(2);
-  const totalPerPerson = alright && ((bill * (1 + tip) / people).toFixed(2));
+  const tipAmount = alright ? (bill * tip / people).toFixed(2) : undefined;
+  const totalPerPerson = alright ? ((bill * (1 + tip) / people).toFixed(2)) : undefined;
   const showTip = alright && !(tipAmount === 'NaN' || tipAmount === 'Infinity');
   const showTotal = alright && !(totalPerPerson === 'NaN' || totalPerPerson === 'Infinity');
 
@@ -57,59 +57,8 @@ function App() {
               }}
             />
 
-            <Label>Select Tip %</Label>
-            <ButtonContainer>
-              <button
-                onClick={() => {
-                  setTip(0.05)
-                }}
-              >
-                5%
-              </button>
+            <Tip setTip={setTip} tip={tip} />
 
-              <button
-                onClick={() => {
-                  setTip(0.1)
-                }}
-              >
-                10%
-              </button>
-
-              <button
-                onClick={() => {
-                  setTip(0.15)
-                }}
-              >
-                15%
-              </button>
-
-              <button
-                onClick={() => {
-                  setTip(0.25)
-                }}
-              >
-                25%
-              </button>
-
-              <button
-                onClick={() => {
-                  setTip(0.5)
-                }}
-              >
-                50%
-              </button>
-              <Input
-                placeholder="Custom"
-                type="number"
-                min={0}
-                value={tip && tip * 100}
-                onChange={(e) => {
-                  if (e.target.value.length < 4){
-                    setTip(e.target.valueAsNumber / 100);
-                  }
-                }}
-              />
-            </ButtonContainer>
             <Label>People</Label>
             <InputWithMargin
               iconType="person"
@@ -134,49 +83,21 @@ function App() {
             {peopleError ? `Can't be zero` : ''}
           </div> */}
 
-          <ResultContainer>
-            <BillContainer style={{ marginBottom: 25}}>
-              <div>
-                <BillName>Tip Amount</BillName>
-                <PerPerson>/ person</PerPerson>
-              </div>
-              <Bill>
-                {showTip ? tipAmount : "$0.00"}
-              </Bill> 
-            </BillContainer>
-            <BillContainer>
-              <div>
-                <BillName>Total</BillName>
-                <PerPerson>/ person</PerPerson>
-              </div>
-              <Bill>
-                {showTotal ? totalPerPerson : "$0.00"}
-              </Bill>
-            </BillContainer>
-            <ResetButton>RESET</ResetButton>
-          </ResultContainer>
+          <Results 
+            showTip={showTip} 
+            showTotal={showTotal} 
+            tipAmount={tipAmount} 
+            totalPerPerson={totalPerPerson} 
+          />
+
         </CalculatorContainer>
       </Container>
     </ThemeProvider>
   );
 }
 
-
 const InputContainer = styled.div`
   padding: 0 8px;
-`;
-
-const BillContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const ResultContainer = styled.div`
-  background-color: ${(props) => props.theme.colors.cyan.dark};
-  width: 100%;
-  border-radius: 15px;
-  padding: 39px 22px 24px 24px;
 `;
 
 const Container = styled.div`
@@ -185,10 +106,6 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
 `;
-
-const ButtonContainer = styled.div`
-  margin-bottom: 32px;
-`
 
 const Image = styled.img`
   padding: 50px 0 40px 0;
